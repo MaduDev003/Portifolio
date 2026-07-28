38b5016dcf6c8fc4e1d43632cba108b1066d8de2
MaduDev003 <mariabevenuto062@gmail.com>
2026-07-27 21:31:35 -0300

    feat: button action

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,11 @@
-import ButtonAction from "../ui/ButtonAction";
+"use client";
+
+import { useState } from "react";
+import { FiDownload } from "react-icons/fi";
+import { ShimmerButton } from "@/components/ui/shimmer-button";
 
 export default function Header() {
+  const [activeLink, setActiveLink] = useState<string>("Home");
   return (
     <header
       className="
@@ -35,19 +40,31 @@ export default function Header() {
             "Experiência",
             "Projetos",
             "Contato",
-          ].map((item) => (
+          ].map((item, index) => (
             <li
-              key={item}
-              className="
-                cursor-pointer
-                font-heading
-                text-zinc-700
-                transition-all
-                duration-200
-                hover:bg-[linear-gradient(135deg,#0F9D7A_0%,#2DD4BF_55%,#7DDC6F_100%)]
-                hover:bg-clip-text
-                hover:text-transparent
-              "
+              onClick={() => setActiveLink(item)}
+              key={index}
+              className={`
+              relative
+              cursor-pointer
+              font-heading
+              transition-all
+              duration-200
+              after:absolute
+              after:left-0
+              after:-bottom-1
+              after:h-0.5
+              after:w-0
+              after:rounded-full
+              after:bg-[linear-gradient(135deg,#0F9D7A_0%,#2DD4BF_55%,#7DDC6F_100%)]
+              after:transition-all
+              after:duration-300
+            text-zinc-600
+              hover:bg-[linear-gradient(135deg,#0F9D7A_0%,#2DD4BF_55%,#7DDC6F_100%)]
+              hover:bg-clip-text
+              hover:text-transparent
+              hover:after:w-full
+            `}
             >
               {item}
             </li>
@@ -56,7 +73,39 @@ export default function Header() {
       </nav>
 
       {/* BOTÃO CURRÍCULO */}
-     <ButtonAction text={"Curriculo"}/>
+       <ShimmerButton
+             shimmerColor="#D1FAE5"
+             shimmerDuration="3s"
+             shimmerSize="0.05em"
+             background="linear-gradient(135deg,rgba(15,157,122,0.75) 0%,rgba(45,212,191,0.65) 55%,rgba(125,220,111,0.55) 100%)"
+             borderRadius="12px"
+             className="
+               h-10
+               px-6
+               font-medium
+               font-heading
+               text-white
+     
+               backdrop-blur-xl
+     
+               border
+               border-emerald-100/30
+     
+               shadow-[0_12px_30px_rgba(15,157,122,.25)]
+     
+               transition-all
+               duration-300
+     
+               hover:-translate-y-0.5
+               hover:border-emerald-100/60
+               hover:shadow-[0_15px_35px_rgba(45,212,191,.35)]
+     
+               active:scale-[0.98]
+             "
+           >
+              <FiDownload  />
+    Currículo
+           </ShimmerButton>
     </header>
   );
 }
--- src/features/aboutMe.tsx
+++ src/features/aboutMe.tsx
@@ -1,14 +1,47 @@
 "use client";
 
-import ButtonAction from "../components/ui/ButtonAction";
+import { ShimmerButton } from "@/components/ui/shimmer-button";
 
 export default function AboutMe() {
   return (
     <section
       id="about"
-      className="min-h-screen  p-4"
+      className="min-h-screen p-4"
     >
-      <ButtonAction text={"Ver Projetos ->"}/>
+      <ShimmerButton
+        shimmerColor="#D1FAE5"
+        shimmerDuration="3s"
+        shimmerSize="0.05em"
+        background="linear-gradient(135deg,rgba(15,157,122,0.75) 0%,rgba(45,212,191,0.65) 55%,rgba(125,220,111,0.55) 100%)"
+        borderRadius="12px"
+        className="
+          h-10
+          px-6
+          font-medium
+          font-heading
+          text-white
+
+          backdrop-blur-xl
+
+          border
+          border-emerald-100/30
+
+          shadow-[0_12px_30px_rgba(15,157,122,.25)]
+
+          transition-all
+          duration-300
+
+          hover:-translate-y-0.5
+          hover:border-emerald-100/60
+          hover:shadow-[0_15px_35px_rgba(45,212,191,.35)]
+
+          active:scale-[0.98]
+        "
+      >
+        <span className="flex items-center gap-2">
+          Ver Projetos →
+        </span>
+      </ShimmerButton>
     </section>
   );
 }
