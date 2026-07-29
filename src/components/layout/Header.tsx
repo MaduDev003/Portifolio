8cd465f5e6a8e416a7fffdb9cba0e7c20447524c
MaduDev003 <mariabevenuto062@gmail.com>
2026-07-29 16:16:02 -0300

    rm: repetied button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -132,18 +132,36 @@ export default function Header() {
           </ShimmerButton>
       </a>
 
-      <SecondaryButton
-          onClick={() =>
-            setTheme(
-              theme === "dark"
-                ? "light"
-                : "dark"
-            )
-          }
-          className="h-9 w-9"
-      >
-          <AnimatedThemeToggler duration={600} fromCenter className={"cursor-pointer"}/>
-      </SecondaryButton>
+      <div
+      className="
+        h-9
+        w-9
+        flex
+        items-center
+        justify-center
+        rounded-full
+        border
+        border-button-secondary-border
+        bg-button-secondary-bg
+        text-button-secondary-text
+        shadow-[0_2px_8px_rgba(15,23,42,0.06)]
+        transition-all
+        duration-300
+        ease-out
+        hover:-translate-y-0.5
+        hover:scale-[1.03]
+        hover:border-button-secondary-hover-border
+        hover:bg-button-secondary-hover-bg
+        hover:text-button-secondary-hover-text
+        hover:shadow-[0_8px_22px_rgba(109,94,247,.14)]
+      "
+    >
+      <AnimatedThemeToggler 
+        duration={400} 
+        fromCenter 
+        className="cursor-pointer"
+      />
+    </div>
 
     </div>
     </header>
