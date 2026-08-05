b01d0e0cb034bc6e171445b0c1a9ca66c788c48f
MaduDev003 <mariabevenuto062@gmail.com>
2026-08-05 14:59:13 -0300

    feat: videos

--- src/features/Projects.tsx
+++ src/features/Projects.tsx
@@ -1,10 +1,13 @@
 "use client";
+
 import { useState } from "react";
-import { FaRegStar } from "react-icons/fa";
+import { FiExternalLink } from "react-icons/fi";
+import { FaGithub, FaRegStar } from "react-icons/fa";
+
 import { projects } from "@/src/data/projects";
+
 export default function Projects() {
-const [hoveredProject,setHoveredProject] = useState<string | null>(null);
-    
+  const [hoveredProject, setHoveredProject] = useState<string | null>(null);
 
   return (
     <section
@@ -12,6 +15,7 @@ const [hoveredProject,setHoveredProject] = useState<string | null>(null);
       className="min-h-screen px-6 py-20 xl:px-34"
     >
       <div className="mx-auto max-w-7xl">
+
         <div className="flex flex-col items-center text-center">
           <span className="font-heading text-5xl font-bold tracking-tight text-brand-middle md:text-6xl">
             Projetos
@@ -24,43 +28,37 @@ const [hoveredProject,setHoveredProject] = useState<string | null>(null);
           </h2>
         </div>
 
+
         <div className="mt-16 grid gap-8 md:grid-cols-2">
+
           {projects.map((project) => (
             <article
+              key={project.title}
               onMouseEnter={() => setHoveredProject(project.title)}
               onMouseLeave={() => setHoveredProject(null)}
-              key={project.title}
-              className="group w-120 h-148 gap-3 rounded-md backdrop:blur-sm border border-border bg-card transition-all duration-300 hover:-translate-y-2 hover:border-brand-middle/40"
+              className="
+                group
+                overflow-hidden
+                rounded-md
+                border
+                border-border
+                bg-card
+                transition-all
+                duration-300
+                hover:-translate-y-2
+                hover:border-brand-middle/40
+                cursor-pointer
+              "
             >
 
-             <div className="relative overflow-hidden rounded-t-md">
-                {project.contrast && (
-                  <span
-                    className="
-                        absolute top-4 right-4 z-10
-                        inline-flex items-center gap-2
-                        overflow-hidden
-                        rounded-full
-                        border border-white/10
-                        bg-linear-to-r from-brand-middle/80 to-brand-hover
-                        px-4 py-2
-                        text-sm font-semibold text-white
-                        shadow-lg
-                        backdrop-blur
-                        transition-all duration-300
-                       
-                    "
-                    >
-                    <FaRegStar className=" group-hover:scale-110 group-hover:rotate-45 transition-all duration-300 relative z-10 text-base" />
-                    <span className="relative z-10">Destaque</span>
-                    </span>
-                )}
+              <div className="relative overflow-hidden">
 
-                {hoveredProject === project.title && (
-                   <video
+                {hoveredProject === project.title ? (
+                  <video
                     src={project.video}
                     autoPlay
                     muted
+                    loop
                     playsInline
                     className="
                       aspect-video
@@ -71,93 +69,231 @@ const [hoveredProject,setHoveredProject] = useState<string | null>(null);
                       group-hover:scale-105
                     "
                   />
-                )}
-                {hoveredProject !== project.title && (
-                <img
+                ) : (
+                  <img
                     src={project.image}
                     alt={project.title}
                     className="
-                    aspect-video
-                    w-full
-                    object-cover
-                    transition
-                    duration-700
-                    group-hover:scale-105
-                    hover:border-brand-middle
+                      aspect-video
+                      w-full
+                      object-cover
+                      transition
+                      duration-700
+                      group-hover:scale-105
                     "
-                />
+                  />
                 )}
-                </div>
 
-              <div className="space-y-5 p-6">
+
+                <div
+                  className="
+                    absolute
+                    inset-0
+                    bg-linear-to-t
+                    from-black/50
+                    via-transparent
+                    to-transparent
+                  "
+                />
+
+
+                {project.contrast && (
+                  <span
+                    className="
+                      absolute
+                      top-4
+                      right-4
+                      z-10
+                      inline-flex
+                      items-center
+                      gap-2
+                      rounded-full
+                      border
+                      border-white/10
+                      bg-linear-to-r
+                      from-brand-middle/80
+                      to-brand-hover
+                      px-4
+                      py-2
+                      text-sm
+                      font-semibold
+                      text-white
+                      shadow-lg
+                      backdrop-blur
+                    "
+                  >
+                    <FaRegStar
+                      className="
+                        text-base
+                        transition-all
+                        duration-300
+                        group-hover:scale-110
+                        group-hover:rotate-45
+                      "
+                    />
+
+                    Destaque
+                  </span>
+                )}
+
+              </div>
+
+
+              <div className="flex flex-col gap-5 p-6">
+
                 <div>
-                  <h3 className="text-2xl font-semibold text-brand-middle/80">
+                  <h3
+                    className="
+                      text-2xl
+                      font-semibold
+                      text-brand-middle/80
+                    "
+                  >
                     {project.title}
                   </h3>
+
 
                   <p className="mt-2 text-muted-foreground">
                     {project.description}
                   </p>
                 </div>
 
+
                 <div className="flex flex-wrap gap-2">
                   {project.technologies.map((tech) => (
                     <span
                       key={tech}
-                      className="rounded-full border border-brand-middle/30 bg-brand-middle/10 px-3 py-1 text-sm text-brand-middle"
+                      className="
+                        rounded-full
+                        border
+                        border-brand-middle/30
+                        bg-brand-middle/10
+                        px-3
+                        py-1
+                        text-sm
+                        text-brand-middle
+                      "
                     >
                       {tech}
                     </span>
                   ))}
                 </div>
 
-               <div className="flex gap-3 py-5">
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={project.demo}
-                        className="
-                        group relative overflow-hidden rounded-xl
-                        bg-linear-to-r from-brand-middle/70 to-brand-hover
-                        px-5 py-2.5
-                        font-semibold text-white
-                        shadow-md shadow-brand-primary/20
-                        transition-all duration-300
-                        hover:-translate-y-1
-                        hover:shadow-xl hover:shadow-brand-primary/40
-                        active:translate-y-0
-                        "
-                    >
-                        <span className="relative z-10">Demo</span>
 
-                    </a>
+                <div
+                  className="
+                    flex
+                    items-center
+                    justify-between
+                    border-t
+                    border-border
+                    pt-5
+                  "
+                >
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={project.demo}
+                    className="
+                      group
+                      inline-flex
+                      items-center
+                      gap-2
+                      rounded-xl
+                      bg-linear-to-r
+                      from-brand-hover/70
+                      to-brand-hover
+                      px-4
+                      py-2
+                      text-md
+                      font-semibold
+                      text-white
+                      shadow-lg
+                      shadow-brand-middle/30
+                      transition-all
+                      duration-300
+                      hover:-translate-y-1
+                    
+                    "
+                  >
+                    Ver projeto
 
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={project.github}
-                        className="
-                        group relative overflow-hidden rounded-xl
-                        border border-border
-                        bg-brand-primary/10 backdrop-blur-sm
-                        px-5 py-2.5
-                        font-semibold
-                        transition-all duration-300
-                        hover:-translate-y-1
-                        hover:border-brand-middle
-                        hover:shadow-lg hover:shadow-brand-primary/20
-                        active:translate-y-0
-                        "
-                    >
-                        <span className="relative z-10">GitHub</span>
+                    <FiExternalLink
+                      size={16}
+                      className="
+                        transition-transform
+                        duration-300
+                        group-hover:translate-x-1
+                        group-hover:-translate-y-1
+                      "
+                    />
+                  </a>
 
-            
-                    </a>
-                    </div>
+
+                  <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={project.github}
+                  className="
+                    group
+                    relative
+                    inline-flex
+                    items-center
+                    gap-2
+                    overflow-hidden
+                    rounded-xl
+                    border
+                    border-muted-foreground/20
+                    bg-brand-hover/10
+                    px-4
+                    py-2
+                    text-md
+                    font-semibold
+                    text-muted-foreground
+                    transition-all
+                    duration-300
+                    hover:-translate-y-1
+                    hover:bg-brand-hover/20
+                  "
+                >
+                  <span
+                    className="
+                      absolute
+                      inset-0
+                      translate-y-full
+                      bg-linear-to-r
+                      from-brand-middle/20
+                      to-brand-hover/20
+                      transition-transform
+                      duration-300
+                      group-hover:translate-y-0
+                    "
+                  />
+
+                  <FaGithub
+                    size={17}
+                    className="
+                      relative
+                      z-10
+                      transition-transform
+                      duration-300
+                      group-hover:scale-110
+                    "
+                  />
+
+                  <span className="relative z-10">
+                    Código
+                  </span>
+                </a>
+                </div>
+
               </div>
+
             </article>
           ))}
+
         </div>
+
       </div>
     </section>
   );
